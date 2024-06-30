87d168eadeee222f488aa197f4d6b18b4a7390be
Retal <saeediretal@gmail.com>
2024-07-01 02:38:33 +0300

    shadows

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,25 +4,30 @@
     </title>
 </svelte:head>
 
-<div class="bg-white h-[30rem] w-[20rem] rounded-3xl grid grid-cols-4 gap-2">
-    <button>%</button>
-    <button>√</button>
-    <button>CE</button>
-    <button>C</button>
+<div class="bg-white h-[30rem] w-[20rem] rounded-3xl grid grid-cols-4 gap 
+p-8 font-bold text-2xl shadow-2xl">
+    <div class="bg-[#2386fe] rounded-full col-span-4 h-14 flex items-center 
+    px-4 mb-3 text-white text-xl">
+123
+    </div>
+    <button class="bg-[#f3f6fb] text-[#717785]">%</button>
+    <button class="bg-[#f3f6fb] text-[#717785]">√</button>
+    <button class="bg-[#f3f6fb] text-[#717785]">CE</button>
+    <button class="bg-[#1f2b55] text-white">C</button>
     <button>7</button>
     <button>8</button>
     <button>9</button>
-    <button>-</button>
+    <button class="bg-[#fe4258] text-white">−</button>
     <button>4</button>
     <button>5</button>
     <button>6</button>
-    <button>÷</button>
+    <button class="bg-[#2088fe] text-white">÷</button>
     <button>1</button>
     <button>2</button>
     <button>3</button>
-    <button>×</button>
+    <button class="bg-[#fec206] text-white">×</button>
     <button>.</button>
     <button>0</button>
-    <button>=</button>
-    <button>+</button>
+    <button class="bg-[#f3f6fb] text-[#717785]">=</button>
+    <button class="bg-[#64dc75] text-white">+</button>
 </div>
